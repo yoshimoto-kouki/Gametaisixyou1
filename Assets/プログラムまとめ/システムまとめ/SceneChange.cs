0074a9447183173f32b82725681d6b0b1b517453
LALU1200 <rarugoori@gmail.com>
2020-01-24 18:11:05 +0900

    取り敢えずTitleーGameーEndのループは作った

---
+++ Assets/プログラムまとめ/システムまとめ/SceneChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : MonoBehaviour
+{
+  
+
+    public void ButtonClickedTitle()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ButtonClickedGame()
+    {
+        SceneManager.LoadScene("End");
+    }
+    public void ButtonClickedEnd()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+
+}
